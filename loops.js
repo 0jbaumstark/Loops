b4789b173ff0f33d19851a52000532d748f0f8d9
0jbaumstark <42749684+0jbaumstark@users.noreply.github.com>
2018-10-22 13:53:24 -0500

    Add files via upload

--- loops.js
+++ loops.js
@@ -1,38 +1,44 @@
-// For-loop functions
-
 function positiveIntSum(limit){
-    let sum = 0
-    for(let i = 1; i <= limit; i ++){
-        sum = sum + i;
+    let sum = 0;
+    for(let k=1; k<=limit; k++){
+        sum = sum + k;
     }
     return sum;
 }
-function additionPatternSum(start, diff, limit){
-    let sum = start
-    for(let k = start ; k <= limit ; k = k++){
-        sum = sum + diff*k
-return diff
- }
-}
-// While-loop function
 
-function multiplicationPatternSum(factor, max, start){
-let sum = start;
-let j = 1; 
-    while (j <= max){
-    sum = sum + factor * j;
-    j = factor * j;
-return j
+
+function addSum(start, diff, max){
+    //setting up varaibles
+    let sum = 0;
+    let currentNumber = start;
+
+    while(currentNumber <= max){
+        //adding to the sum.
+        sum = sum + currentNumber;
+        //moving to the next number in the list.
+        currentNumber = currentNumber + diff;
     }
+
+    return sum;
 }
 
+function multSum(start, factor, max){
+    //setting up varaibles
+    let sum = 0;
+    let currentNumber = start;
 
+    while(currentNumber <= max){
+        //adding to the sum.
+        sum = sum + currentNumber;
+        //moving to the next number in the list.
+        currentNumber = currentNumber * factor;
+    }
 
-//Later copy paste the checking code below here.
-
-console.log("question 1: "+ positiveIntSum(1500));
-console.log("question 2: "+ positiveIntSum(2000000));
-console.log("question 3: "+ additionPatternSum(-80,60,50));
-console.log("question 4: "+ additionPatternSum(4,8,50000));
-console.log("question 5: "+ multiplicationPatternSum());
-console.log("question 6: "+ multiplicationPatternSum());
+    return sum;
+}
+console.log(positiveIntSum(1500));
+console.log(positiveIntSum(2000000));
+console.log(addSum(-80, 60, 2860));
+console.log(addSum(4, 12, 50000));
+console.log(multSum(7, 2, 50000));
+console.log(multSum(2, -3, 1000000));
